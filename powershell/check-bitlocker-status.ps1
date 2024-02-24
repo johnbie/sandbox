1de8e42dd48675724988b5dc2254e20a7ba43c38
John Lee <johnqwerty.lee1@gmail.com>
2024-02-23 16:21:25 -0800

    Added permission and docs

--- powershell/check-bitlocker-status.ps1
+++ powershell/check-bitlocker-status.ps1
@@ -1,1 +1,4 @@
 manage-bde -status C:
+# https://www.tenforums.com/tutorials/36901-check-bitlocker-drive-encryption-status-windows-10-a.html
+# https://www.howtogeek.com/805225/disable-bitlocker/
+# https://www.tenforums.com/antivirus-firewalls-system-security/36377-can-i-shut-down-my-pc-while-bitlocker-encrypting.html
