465eeb0dd64962510b129a2d818a243ea8f29433
John Lee <johnqwerty.lee1@gmail.com>
2024-03-12 11:40:01 -0700

    File permissions check

--- bash/conditionals.sh
+++ bash/conditionals.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+# string conditions
 val="a"
 echo $(("$val" == "a")) # [[ "$val" == "a" ]]
 echo $(("$val" != "a")) # [[ "$val" != "a" ]]
 
+# numeric conditions
 num=1
 [[ "$num" -eq 1 ]] # equals
 [[ "$num" -ne 1 ]] # not equal
@@ -10,3 +13,9 @@ num=1
 [[ "$num" -le 1 ]] # less than or equal to
 [[ "$num" -gt 1 ]] # greater than
 [[ "$num" -ge 1 ]] # greater than or equal to
+
+# file permissions
+file = "./hello"
+[[ -r $file ]]
+[[ -w $file ]]
+[[ -x $file ]]
